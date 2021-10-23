c59ec02d8a3f31a3275f742e51e3ad9e28384fb4
longphamit <longphamjustdoit@gmail.com>
2021-10-23 10:06:21 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,42 @@
+import { Button, Checkbox, Input,Form } from 'antd'
+import _fetch from 'isomorphic-fetch';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const onFinish = (values) => {
+    const data={
+      fullName: values.fullname,
+      email: values.email,
+      password: values.password,
+      phone: values.phone,
+      platform: "WEB",
+      url_active_account: "123"
+    };
+    console.log(data)
+    _fetch('https://api-dev.ecomos.vn:8443/gateway-service/v1/customers/register/UAUAP',{
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(data)
+  })
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    })
+    .then(function(stories) {
+        console.log(stories);
+    });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +46,62 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Họ và tên"
+        name="fullname"
+        rules={[{ required: true, message: 'Hãy nhập họ và tên!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="email"
+        name="email"
+        rules={[{ required: true, message: 'Hãy nhập email!' }]}
+      >
+        <Input type="email" />
+      </Form.Item>
+      <Form.Item
+        label="phone"
+        name="phone"
+        rules={[{ required: true, message: 'Hãy nhập số điện thoại!' }]}
+      >
+        <Input />
+      </Form.Item>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        label="Re Password"
+        name="rePass"
+        rules={[{ required: true, message: 'Please input your password again!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
       </main>
 
       <footer className={styles.footer}>
